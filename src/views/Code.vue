57b3639bb9fada3f49997ba661bd2cc0af881d9c
MayDragon <102609887+maydragon2000@users.noreply.github.com>
2024-06-25 22:26:44 -0400

    fixed Eslint Issue

--- src/views/Code.vue
+++ src/views/Code.vue
@@ -1,60 +1,84 @@
 <template>
-    <div>
+  <div>
     <div>{{ query }}</div>
     <button @click="getTokens" class="button">Get Tokens</button>
     <div v-if="idToken.length > 0">
-    <label>ID Token</label>
-    <div class="token" >{{ idToken }} </div>
-    <label>Access Token</label>
-    <div class="token" >{{ accessToken }} </div>
-    <button class="button" @click="callApi()">Call API</button>
+      <label>ID Token</label>
+      <div class="token">{{ idToken }}</div>
+      <label>Access Token</label>
+      <div class="token">{{ accessToken }}</div>
+      <button class="button" @click="callApi()">Call API</button>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-const qs = require('querystring');
-
-const pool = require('@/assets/pool-config.js')
-const axios = require('axios')
+const qs = require("querystring");
+const pool = require("@/assets/pool-config.js");
+const axios = require("axios");
 
 export default {
-    props: ['query'],
-    data: function () {
-        return {
-            accessToken: '',
-            idToken: '',
-            decodedIdToken: undefined,
-            decodedAccessToken: undefined
-        }
+  props: ["query"],
+  data() {
+    return {
+      accessToken: "",
+      idToken: "",
+      decodedIdToken: undefined,
+      decodedAccessToken: undefined,
+    };
+  },
+  methods: {
+    getTokens() {
+      axios
+        .post(
+          `${pool.HOSTED_UI}/oauth2/token`,
+          qs.stringify({
+            grant_type: "authorization_code",
+            client_id: pool.CLIENTID,
+            scope: "openid",
+            code: this.query.code,
+            redirect_uri: pool.HTTPS_REDIRECT_GW_URI,
+          }),
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        )
+        .then((res) => {
+          this.accessToken = res.data.access_token;
+          this.idToken = res.data.id_token;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.error("Some bug information", error);
+        });
     },
-    template: '',
-    methods: {
-        getTokens: function () {
-            let self = this
-            axios.post(pool.HOSTED_UI+'/oauth2/token', qs.stringify({
-                grant_type: 'authorization_code', 
-                client_id: pool.CLIENTID, 
-                scope: 'openid', 
-                code: this.query.code,
-                redirect_uri: pool.HTTPS_REDIRECT_GW_URI
-            }), {
-                headers: {
-                    "Content-Type": 'application/x-www-form-urlencoded'
-                } 
-            }).then((res) => {
-                self.accessToken = res.data.access_token
-                self.idToken = res.data.id_token
-            }).catch(console.log)
-        },
-        callApi: function() {
-            var api_gateway_url = 'https://xxxxxxxxxxx.execute-api.eu-west-1.amazonaws.com/demo/';
-            axios.get(api_gateway_url, {
-                    headers: { 'Authorization' : this.idToken }
-            }).then((data) => console.log(data)).catch((err) => console.log(err))
-
-        }
-       
-    } 
-}
+    callApi() {
+      const api_gateway_url =
+        "https://xxxxxxxxxxx.execute-api.eu-west-1.amazonaws.com/demo/";
+      axios
+        .get(api_gateway_url, {
+          headers: { Authorization: this.idToken },
+        })
+        .then((res) => {
+          // eslint-disable-next-line no-console
+          console.log(res.data);
+        })
+        .catch((err) => {
+          // eslint-disable-next-line no-console
+          console.error("this is error", err);
+        });
+    },
+  },
+};
 </script>
+
+<!-- <style scoped>
+  .button {
+    /* Add your styles here */
+  }
+  .token {
+    /* Add your styles here */
+  }
+  </style> -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="home">
-     <a class="button button-primary" :href="cognitoLogin">Login with Cognito Hosted UI</a>
+    <a class="button button-primary" :href="cognitoLogin"
+      >Login with Cognito Hosted UI</a
+    >
+    <div>
+      <a href="https://t.me/oxsnm">My telegram:</a>
+      <br />
+      <a href=" https://github.com/maydragon2000/"> My github:</a>
+      <br />
+      Let's chat on above platform if you intend to meet me in forward.
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-const pool = require('@/assets/pool-config.js')
-const qs = require('querystring')
+const pool = require("@/assets/pool-config.js");
+const qs = require("querystring");
 
 export default {
-  name: 'Home',
+  name: "Home",
   computed: {
     cognitoLogin: function () {
-      var query = {}
-      query.redirect_uri = pool.HTTPS_REDIRECT_GW_URI
-      query.scope = 'openid'
-      query.response_type = 'code'
-      query.client_id = pool.CLIENTID
-      var uri = pool.HOSTED_UI + '/login?' + qs.stringify(query)
-      return uri
-    }
-  }
-}
+      var query = {};
+      query.redirect_uri = pool.HTTPS_REDIRECT_GW_URI;
+      query.scope = "openid";
+      query.response_type = "code";
+      query.client_id = pool.CLIENTID;
+      var uri = pool.HOSTED_UI + "/login?" + qs.stringify(query);
+      return uri;
+    },
+  },
+};
 </script>
